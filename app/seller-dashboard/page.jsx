7c00a8e450ffc08b0cf1c50587f85ddfb8e2ddd7
Ashish Prakash <asoftwareguy11@gmail.com>
2023-10-03 06:08:06 +0530

    finished/seller-dashboard

--- app/seller-dashboard/page.jsx
+++ app/seller-dashboard/page.jsx
@@ -16,6 +16,7 @@ import {
   Button,
   Flex,
   Heading,
+  Image,
   List,
   ListIcon,
   ListItem,
@@ -80,7 +81,6 @@ const page = () => {
                 - Total 2 ($200)
               </Heading>
             </Flex>
-
             <Link href={"/orders"}>View all</Link>
           </Flex>
           {/*My Works*/}
@@ -93,6 +93,261 @@ const page = () => {
             </Flex>
 
             <Link href={"/orders"}>View all</Link>
+          </Flex>
+          {/* 4 Step Process */}
+          <Flex className="w-full flex-col bg-white p-5 gap-5">
+            <List>
+              <ListItem>
+                <Flex
+                  align="center"
+                  justify="space-evenly"
+                  borderBottom={"1px solid rgb(133, 133, 133)"}
+                  className="flex-col sm:flex-row mt-6 p-5 bg-white"
+                >
+                  {/* Image Container */}
+                  <Flex
+                    borderRadius="sm"
+                    overflow="hidden"
+                    position="relative"
+                    className="m-1 sm:order-last"
+                  >
+                    <Image
+                      src={"https://cdn.kwork.com/images/index/kwork-steps.svg"}
+                      alt=""
+                      objectFit="cover"
+                      className="w-full h-60 sm:w-80"
+                    />
+                  </Flex>
+                  {/*Details Container*/}
+                  <Box className="w-full sm:w-5/6 px-4">
+                    <Text className="mt-4 sm:mt-10 font-bold text-lg">
+                      Four Simple Steps to Workpido Success
+                    </Text>
+                    <Text className="mt-2">
+                      {`The secret to success on Workpido is starting off on the right foot and actively maintaining your ratings. Here are some tips to help you become a top seller on Workpido.`}
+                    </Text>
+                  </Box>
+                </Flex>
+              </ListItem>
+              <ListItem>
+                <Flex
+                  align="start"
+                  justify="space-evenly"
+                  borderBottom={"1px solid rgb(133, 133, 133)"}
+                  className="flex-col md:flex-row mt-3 pb-3"
+                >
+                  {/* Image Container */}
+                  <Flex
+                    borderRadius="sm"
+                    overflow="hidden"
+                    className="m-1 items-start"
+                  >
+                    <Image
+                      src={"https://cdn.kwork.com/images/index/magic-wand.svg"}
+                      alt=""
+                      objectFit="cover"
+                      className="w-24 h-24"
+                    />
+                    <Box className="w-full md:w-3/4 px-4">
+                      <Text className="font-bold text-lg">
+                        Step 1: Create your kworks—the more, the better
+                      </Text>
+                    </Box>
+                  </Flex>
+                  {/* Button Container */}
+                  <Flex
+                    flexDirection="row"
+                    align="center"
+                    justify="space-between"
+                    className="mt-4 sm:justify-center sm:mx-3"
+                  >
+                    <Button
+                      width={"100%"}
+                      as={"a"}
+                      display={"inline-flex"}
+                      fontSize={"sm"}
+                      fontWeight={600}
+                      border={"1px"}
+                      borderColor={"brand.primary"}
+                      color={"brand.primary"}
+                      bg={"white"}
+                      href={"/how-to-earn"}
+                      _hover={{
+                        bg: "brand.primary",
+                        color: "white",
+                      }}
+                      className="m-3"
+                    >
+                      Learn More
+                    </Button>
+                  </Flex>
+                </Flex>
+              </ListItem>
+              <ListItem>
+                <Flex
+                  align="start"
+                  justify="space-evenly"
+                  borderBottom={"1px solid rgb(133, 133, 133)"}
+                  className="flex-col md:flex-row mt-3 pb-3"
+                >
+                  {/* Image Container */}
+                  <Flex
+                    borderRadius="sm"
+                    overflow="hidden"
+                    className="m-1 items-start"
+                  >
+                    <Image
+                      src={"https://cdn.kwork.com/images/index/magic-wand.svg"}
+                      alt=""
+                      objectFit="cover"
+                      className="w-24 h-24"
+                    />
+                    <Box className="w-full md:w-3/4 px-4">
+                      <Text className="font-bold text-lg">
+                        Step 2: Share your kworks to get your first orders and
+                        reviews!
+                      </Text>
+                    </Box>
+                  </Flex>
+                  {/* Button Container */}
+                  <Flex
+                    flexDirection="row"
+                    align="center"
+                    justify="space-between"
+                    className="mt-4 sm:justify-center sm:mx-3"
+                  >
+                    <Button
+                      width={"100%"}
+                      as={"a"}
+                      display={"inline-flex"}
+                      fontSize={"sm"}
+                      fontWeight={600}
+                      border={"1px"}
+                      borderColor={"brand.primary"}
+                      color={"brand.primary"}
+                      bg={"white"}
+                      href={"/how-to-earn"}
+                      _hover={{
+                        bg: "brand.primary",
+                        color: "white",
+                      }}
+                      className="m-3"
+                    >
+                      Learn More
+                    </Button>
+                  </Flex>
+                </Flex>
+              </ListItem>
+              <ListItem>
+                <Flex
+                  align="start"
+                  justify="space-between"
+                  borderBottom={"1px solid rgb(133, 133, 133)"}
+                  className="flex-col md:flex-row mt-3 pb-3"
+                >
+                  {/* Image Container */}
+                  <Flex
+                    borderRadius="sm"
+                    overflow="hidden"
+                    className="m-1 items-start justify-start"
+                  >
+                    <Image
+                      src={"https://cdn.kwork.com/images/index/smartphone.svg"}
+                      alt=""
+                      objectFit="cover"
+                      className="w-24 h-24"
+                    />
+                    <Box className="w-full md:w-3/4 px-4">
+                      <Text className="font-bold text-lg">
+                        Step 3: Get the Kwork app
+                      </Text>
+                    </Box>
+                  </Flex>
+                  {/* Button Container */}
+                  <Flex
+                    flexDirection="row"
+                    align="center"
+                    justify="space-between"
+                    className="mt-4 sm:justify-center sm:mx-3"
+                  >
+                    <Button
+                      width={"100%"}
+                      as={"a"}
+                      display={"inline-flex"}
+                      fontSize={"sm"}
+                      fontWeight={600}
+                      border={"1px"}
+                      borderColor={"brand.primary"}
+                      color={"brand.primary"}
+                      bg={"white"}
+                      href={"/how-to-earn"}
+                      _hover={{
+                        bg: "brand.primary",
+                        color: "white",
+                      }}
+                      className="m-3"
+                    >
+                      Learn More
+                    </Button>
+                  </Flex>
+                </Flex>
+              </ListItem>
+              <ListItem>
+                <Flex
+                  align="start"
+                  justify="space-evenly"
+                  borderBottom={"1px solid rgb(133, 133, 133)"}
+                  className="flex-col md:flex-row mt-3 pb-3"
+                >
+                  {/* Image Container */}
+                  <Flex
+                    borderRadius="sm"
+                    overflow="hidden"
+                    className="m-1 items-start"
+                  >
+                    <Image
+                      src={"https://cdn.kwork.com/images/index/hat.svg"}
+                      alt=""
+                      objectFit="cover"
+                      className="w-24 h-24"
+                    />
+                    <Box className="w-full md:w-3/4 px-4">
+                      <Text className="font-bold text-lg">
+                        Step 4: Take our free introductory course How To Earn on
+                        Kwork
+                      </Text>
+                    </Box>
+                  </Flex>
+                  {/* Button Container */}
+                  <Flex
+                    flexDirection="row"
+                    align="center"
+                    justify="space-between"
+                    className="mt-4 sm:justify-center sm:mx-3"
+                  >
+                    <Button
+                      width={"100%"}
+                      as={"a"}
+                      display={"inline-flex"}
+                      fontSize={"sm"}
+                      fontWeight={600}
+                      border={"1px"}
+                      borderColor={"brand.primary"}
+                      color={"brand.primary"}
+                      bg={"white"}
+                      href={"/how-to-earn"}
+                      _hover={{
+                        bg: "brand.primary",
+                        color: "white",
+                      }}
+                      className="m-3"
+                    >
+                      Learn More
+                    </Button>
+                  </Flex>
+                </Flex>
+              </ListItem>
+            </List>
           </Flex>
         </Flex>
       </Flex>
