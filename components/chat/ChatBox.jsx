02a4597fa354bcbddc2bfea52166c020be11f4d4
DezyNation <87603422+DezyNation@users.noreply.github.com>
2024-01-12 01:19:58 +0530

    chat UI fixes

--- components/chat/ChatBox.jsx
+++ components/chat/ChatBox.jsx
@@ -4,11 +4,26 @@ import {
   Avatar,
   Box,
   Button,
+  FormLabel,
   HStack,
   IconButton,
   Image,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Select,
   Spacer,
+  Stack,
   Text,
+  Textarea,
 } from "@chakra-ui/react";
 import { InputBox } from "./InputBox";
 import { FiSearch } from "react-icons/fi";
@@ -21,14 +36,20 @@ import {
   CellMeasurerCache,
 } from "react-virtualized";
 import Link from "next/link";
+import useAuth from "@/hooks/useAuth";
 
 export const ChatBox = ({ messages, addMessage, receiver, isTyping }) => {
+  const { user } = useAuth();
+  const [order, setOrder] = useState({
+    status: false,
+    description: "",
+    price: "",
+  });
+
   const cache = new CellMeasurerCache({
     fixedWidth: true,
     defaultHeight: 50,
   });
-
-  
 
   const rowRenderer = ({ index, key, parent, style }) => (
     <CellMeasurer
@@ -77,7 +98,7 @@ export const ChatBox = ({ messages, addMessage, receiver, isTyping }) => {
             </Box>
           ) : null}
         </Box>
-        <Text fontSize={"xs"} color={"#e2e2e2"}>
+        <Text fontSize={"xs"} color={"#a2a2a2"}>
           {`${new Date(
             messages[index]?.timestamp ?? messages[index]?.createdAt
           ).getDate()} ${monthNames[
@@ -97,25 +118,24 @@ export const ChatBox = ({ messages, addMessage, receiver, isTyping }) => {
           <Avatar size={"sm"} name={receiver?.username} />
           <Box>
             <Text fontSize="12px" fontWeight={"semibold"}>
-              {isTyping
-                ? "Typing..."
-                : receiver?.displayName || receiver?.username}
+              {receiver?.displayName || receiver?.username}
             </Text>
             <Text fontSize="8px" fontWeight={"medium"} color={"gray.500"}>
-              {receiver?.profession}
+              {isTyping ? "Typing..." : receiver?.profession}
             </Text>
           </Box>
           <Spacer />
-          {/* <HStack gap={4}>
-            <FiSearch size={20} cursor={"pointer"} color={"#e2e2e2"} />
+          <HStack gap={4}>
+            <FiSearch size={20} cursor={"pointer"} color={"#a2a2a2"} />
             <Button
               size={"sm"}
               colorScheme="whatsapp"
               leftIcon={<FaCirclePlus />}
+              onClick={() => setOrder({ ...order, status: true })}
             >
               Order
             </Button>
-          </HStack> */}
+          </HStack>
         </HStack>
       </Box>
       <Box h={"80%"} overflowY={"scroll"} overflowX={"hidden"}>
@@ -132,11 +152,83 @@ export const ChatBox = ({ messages, addMessage, receiver, isTyping }) => {
         </AutoSizer>
       </Box>
       <Box px={3}>
-        <InputBox
-          addMessage={addMessage}
-          receiver={receiver?.username}
-        />
+        <InputBox addMessage={addMessage} receiver={receiver?.username} />
       </Box>
+
+      <Modal size={["full", "3xl"]} isOpen={order?.status} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader fontWeight={"medium"}>Send Custom Order</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>Send a custom offer to your buyer.</Text>
+            <Text py={2}>
+              1. Describe the services that you are offering, along with the
+              steps you will take to complete the client's project successfully.
+            </Text>
+            <Text>
+              2. Describe your related experience, including 1-3 similar
+              projects you've worked on.
+            </Text>
+            <Textarea
+              mt={4}
+              value={order?.description}
+              onChange={(e) =>
+                setOrder({ ...order, description: e.target.value })
+              }
+              w={"full"}
+              h={48}
+              resize={"none"}
+              placeholder="Write about your offer here..."
+              fontSize={"sm"}
+            />
+            <br />
+            <Stack
+              direction={["column", "row"]}
+              alignItems={"center"}
+              justifyContent={"space-between"}
+              mt={6}
+              mb={3}
+            >
+              <Select
+                placeholder="Select category"
+                maxW={["full", "xs"]}
+              ></Select>
+              <Select
+                placeholder="Select subcategory"
+                maxW={["full", "xs"]}
+              ></Select>
+            </Stack>
+            <Box mt={6} mb={3}>
+              <FormLabel fontWeight={"medium"}>Price</FormLabel>
+              <InputGroup maxW={["full", "xs"]}>
+                <InputLeftElement children={user?.currency?.label ?? "₹"} />
+                <Input placeholder="10-1000" min={10} />
+              </InputGroup>
+            </Box>
+            <Box mt={6} mb={3}>
+              <FormLabel fontWeight={"medium"}>Delivery</FormLabel>
+              <InputGroup maxW={["full", "xs"]}>
+                <Input />
+                <InputRightElement children={"Days"} paddingRight={6} />
+              </InputGroup>
+            </Box>
+          </ModalBody>
+          <ModalFooter>
+            <HStack>
+              <Button
+                variant="outline"
+                onClick={() => setOrder({ status: false })}
+              >
+                Cancel
+              </Button>
+              <Button colorScheme="green" bgColor={"brand.primary"}>
+                Send
+              </Button>
+            </HStack>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
   );
 };
