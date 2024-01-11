34f1087fbe7e4e5f8a313bddb59411abffa972de
DezyNation <87603422+DezyNation@users.noreply.github.com>
2024-01-11 17:27:55 +0530

    settings page added

--- app/edit-profile/page.jsx
+++ app/edit-profile/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import useAuth from "@/hooks/useAuth";
 import { API } from "@/lib/api";
-import { COUNTRIES } from "@/lib/constants";
+import { CLIENT_BASE_URL, COUNTRIES } from "@/lib/constants";
 import {
   Box,
   Button,
@@ -22,6 +22,7 @@ import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import Link from "next/link";
 
 const EditProfile = () => {
   const { avatarUrl, me, user } = useAuth();
@@ -47,6 +48,9 @@ const EditProfile = () => {
 
   const Formik = useFormik({
     initialValues: {
+      username: user?.username,
+      phone: user?.phone,
+      email: user?.email,
       displayName: user?.displayName,
       profession: user?.profession,
       acceptingOrders: user?.acceptingOrders,
@@ -93,7 +97,7 @@ const EditProfile = () => {
           Settings
         </Text>
 
-        <Tabs colorScheme="yellow">
+        <Tabs colorScheme="yellow" maxW={["full", "2xl"]}>
           <TabList>
             <Tab>General</Tab>
             <Tab>Profile</Tab>
@@ -103,50 +107,128 @@ const EditProfile = () => {
           <TabPanels>
             <TabPanel>
               <Box p={4} bgColor={"#FFF"} rounded={4} boxShadow={"base"}>
-                General
+                <Stack
+                  direction={["column", "row"]}
+                  alignItems={"center"}
+                  justifyContent={"flex-start"}
+                >
+                  <Text>Your profile link - </Text>
+                  <Link href={CLIENT_BASE_URL + `/profile/${user?.username}`}>
+                    <Text color={"twitter.600"}>
+                      {CLIENT_BASE_URL + `/profile/${user?.username}`}
+                    </Text>
+                  </Link>
+                </Stack>
+                <br />
+                <FormControl pb={8}>
+                  <FormLabel fontSize={"sm"}>Username</FormLabel>
+                  <Input
+                    placeholder="Your Username (All lower case, without spaces or special charachters)"
+                    name="username"
+                    value={Formik.values.username}
+                    onChange={Formik.handleChange}
+                  />
+                </FormControl>
+                <FormControl pb={8} w={["full", "xs"]}>
+                  <FormLabel fontSize={"sm"}>Phone Number</FormLabel>
+                  <Input
+                    placeholder="Phone No."
+                    type="tel"
+                    name="phone"
+                    value={Formik.values.phone}
+                    onChange={Formik.handleChange}
+                  />
+                </FormControl>
+                <FormControl pb={8} w={["full"]}>
+                  <FormLabel fontSize={"sm"}>Email</FormLabel>
+                  <Input
+                    placeholder="Your Email"
+                    type="email"
+                    name="email"
+                    value={Formik.values.email}
+                    onChange={Formik.handleChange}
+                  />
+                </FormControl>
+                <HStack gap={6}>
+                  <FormControl pb={8} w={["full"]}>
+                    <FormLabel fontSize={"sm"}>New Password</FormLabel>
+                    <Input name="newPassword" placeholder="New Password" />
+                  </FormControl>
+                  <FormControl pb={8} w={["full"]}>
+                    <FormLabel fontSize={"sm"}>Confirm Password</FormLabel>
+                    <Input
+                      name="confirmPassword"
+                      placeholder="Confirm Password"
+                      type="password"
+                    />
+                  </FormControl>
+                </HStack>
+                <br />
+                <HStack w={"full"} justifyContent={"flex-end"}>
+                  <Button
+                    onClick={() => push("/profile/me")}
+                    fontSize={"sm"}
+                    fontWeight={"normal"}
+                  >
+                    Back to Profile
+                  </Button>
+                  <Button
+                    colorScheme="yellow"
+                    onClick={Formik.handleSubmit}
+                    fontSize={"sm"}
+                    fontWeight={"normal"}
+                  >
+                    Save Details
+                  </Button>
+                </HStack>
               </Box>
             </TabPanel>
             <TabPanel>
               <Box p={4} bgColor={"#FFF"} rounded={4} boxShadow={"base"}>
-                <Stack
-                  direction={["column", "row"]}
-                  w={"full"}
-                  gap={8}
-                  alignItems={"flex-start"}
-                >
+                <Box w={"full"}>
+                  <FormControl w={["full", "sm"]}>
+                    <FormLabel>Display Name</FormLabel>
+                    <Input
+                      name="displayName"
+                      value={Formik.values.displayName}
+                      onChange={Formik.handleChange}
+                      placeholder="Eg, Akshay Singh or SEO Blade Agency"
+                    />
+                  </FormControl>
+                  <br />
                   <Box w={["full", "xs"]}>
-                    <Image src={avatarUrl} boxSize={"xs"} objectFit={"cover"} />
-                    <HStack py={2} w={"full"} justifyContent={"center"}>
-                      <Button size={"sm"}>Change</Button>
-                      <Button size={"sm"} colorScheme="red">
+                    <Image
+                      src={avatarUrl}
+                      width={48}
+                      height={48}
+                      objectFit={"contain"}
+                    />
+                    <HStack py={2} w={"full"} justifyContent={"flex-start"}>
+                      <Button
+                        variant={"ghost"}
+                        size={"sm"}
+                        colorScheme="twitter"
+                      >
+                        Change
+                      </Button>
+                      <Button variant={"ghost"} size={"sm"} colorScheme="red">
                         Remove
                       </Button>
                     </HStack>
                   </Box>
-                  <Box w={"full"}>
-                    <FormControl w={["full", "sm"]}>
-                      <FormLabel>Display Name</FormLabel>
-                      <Input
-                        name="displayName"
-                        value={Formik.values.displayName}
-                        onChange={Formik.handleChange}
-                        placeholder="Eg, Akshay Singh or SEO Blade Agency"
-                      />
-                    </FormControl>
-                    <br />
-                    <FormControl w={["full", "full"]}>
-                      <FormLabel>Bio</FormLabel>
-                      <Textarea
-                        placeholder="Tell us something about yourself..."
-                        h={"48"}
-                        resize={"none"}
-                        name="bio"
-                        value={Formik.values.bio}
-                        onChange={Formik.handleChange}
-                      />
-                    </FormControl>
-                  </Box>
-                </Stack>
+                  <br />
+                  <FormControl w={["full", "full"]}>
+                    <FormLabel>About Yourself</FormLabel>
+                    <Textarea
+                      placeholder="Tell us something about yourself..."
+                      h={"48"}
+                      resize={"none"}
+                      name="bio"
+                      value={Formik.values.bio}
+                      onChange={Formik.handleChange}
+                    />
+                  </FormControl>
+                </Box>
                 <br />
                 <br />
                 <br />
@@ -156,8 +238,8 @@ const EditProfile = () => {
                   gap={8}
                   alignItems={"flex-start"}
                 >
-                  <FormControl w={["full", "sm"]}>
-                    <FormLabel>Profession</FormLabel>
+                  <FormControl w={["full"]}>
+                    <FormLabel>What do you do?</FormLabel>
                     <Input
                       name="profession"
                       value={Formik.values.profession}
@@ -165,6 +247,15 @@ const EditProfile = () => {
                       placeholder="eg, Graphic Designer"
                     />
                   </FormControl>
+                </Stack>
+                <br />
+                <br />
+                <Stack
+                  direction={["column", "row"]}
+                  w={"full"}
+                  gap={8}
+                  alignItems={"flex-start"}
+                >
                   <FormControl w={["full", "sm"]}>
                     <FormLabel>Country</FormLabel>
                     <Select
@@ -179,7 +270,6 @@ const EditProfile = () => {
                       useBasicStyles
                     />
                   </FormControl>
-
                   <FormControl w={["full", "sm"]}>
                     <FormLabel>Currency</FormLabel>
                     <ChakraSelect
@@ -333,7 +423,7 @@ const EditProfile = () => {
 
                 <br />
                 <br />
-                <HStack w={"full"} p={[4, 8]} justifyContent={"flex-end"}>
+                <HStack w={"full"} justifyContent={"flex-end"}>
                   <Button
                     onClick={() => push("/profile/me")}
                     fontSize={"sm"}
